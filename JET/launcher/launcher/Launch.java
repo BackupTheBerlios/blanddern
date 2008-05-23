3fc414fb4bc49fac143a3b6d46b7f082ee1c05aa
scheerj <scheerj>
2008-05-23 10:30:16 +0000

    Check of the genmodel file extension and existence.

--- JET/launcher/launcher/Launch.java
+++ JET/launcher/launcher/Launch.java
@@ -39,6 +39,7 @@ import patternsEngine.ItfPatternEngine;
 import patternsEngine.PatternEngine;
 
 import emf2prolog.UML21ToPrologV6;
+import filtering.MyFilter;
 
 import translated.AdaptorGeneration;
 import utils.Tools;
@@ -55,7 +56,6 @@ import adaptor.*;
 public class Launch{
 	public static void main(String[] args){
 		BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
-		// TODO List aspectFiles = new ArrayList();
 		
 		/* .aj and prolog files directories creation */
 		File ajDir = new File("src-gen/");
@@ -99,25 +99,33 @@ public class Launch{
 		/* FileChooser used for the choice of the genmodel file corresponding
 		 * to the target metamodel (filter on .genmodel extension)
 		 */
-		/*TODO plante une fois sur deux : le prog ne se termine pas
-		 * TODO comment griser la zone de saisie ?
+		/*TODO voir s'il est possible d'enlever le filtre acceptAll
 		 */
+		//FileFilter acceptAll = choix.getAcceptAllFileFilter();
+		//choix.removeChoosableFileFilter(acceptAll);
 		/*String genmodel = "";
 		JFileChooser choix = new JFileChooser();
-		FileFilter acceptAll = choix.getAcceptAllFileFilter();
-		choix.removeChoosableFileFilter(acceptAll);
 		choix.setFileFilter(new MyFilter(new String[]{"genmodel"}, "*.genmodel"));
 		
 		JOptionPane.showMessageDialog(null, "Indiquez le fichier genModel du métamodèle cible");
 		int retour = choix.showOpenDialog(null);
 		if(retour == JFileChooser.APPROVE_OPTION) {
 			genmodel = choix.getSelectedFile().getAbsolutePath();
+			
+			if(!genmodel.substring(genmodel.lastIndexOf('.')+1).equalsIgnoreCase("genmodel")){
+				System.err.println("Le fichier sélectionné doit être de type .genmodel");
+				System.exit(-1);
+			}
 		}else{
 			JOptionPane.showMessageDialog(null, "Pas de fichier genModel choisi");
 			System.exit(-1);
+		}
+		
+		File genFile = new File(genmodel);
+		if(!genFile.exists()){
+			System.err.println("Le fichier genmodel n'existe pas");
+			System.exit(-1);
 		}*/
-		
-		
 		
 		/* the package where the Impl files are stored is sought */
 		ImplFinder implLoc = new ImplFinder(new File(/*genmodel*/"../Lanterne/metamodel/lanterne.genmodel"));
@@ -147,7 +155,6 @@ public class Launch{
 				
 				File file = new File("src-gen/"+c.getName()+".aj");
 				Tools.saveGenerated(result, file);
-				// TODO aspectFiles.add(file);
 			}
 			
 			launchAspect();
