099a119976f5951b995e486477f0816c486b29f4
scheerj <scheerj>
2008-05-28 11:36:28 +0000

    First commit of the factory that will adapt the source model into target model by using the pattern filter.

---
+++ JET/launcher/filtering/AdaptedFactory.java
@@ -0,0 +1,27 @@
+package filtering;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.emf.ecore.EObject;
+
+import patternsEngine.ItfPatternEngine;
+
+public class AdaptedFactory {
+	ItfPatternEngine pe;
+	
+	public AdaptedFactory(ItfPatternEngine pe){
+		this.pe = pe;
+	}
+	
+	EObject[] loadModel(EObject[] srcModel){
+		List ret = new ArrayList();
+		PatternFilter pf = new PatternFilter(pe);
+		
+		for(int i=0; i<srcModel.length; i++){
+			ret.add(pf.source2Target(srcModel[i]));
+		}
+		
+		return (EObject[])ret.toArray();
+	}
+}
