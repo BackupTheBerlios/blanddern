7369d3dced652703e63bc3f58ec5b949fe69c1d9
scheerj <scheerj>
2008-05-29 15:24:58 +0000

    Generates the Set of Maps that will match the proxies names with the corresponding EObjects from the List of results returned by the prolog interpreter.

--- JET/launcher/patternsEngine/PatternEngine.java
+++ JET/launcher/patternsEngine/PatternEngine.java
@@ -1,7 +1,9 @@
 package patternsEngine;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -34,10 +36,53 @@ public class PatternEngine implements ItfPatternEngine{
 		if(id2obj!=null){
 			File programFile = new File("prologFiles/sourceModel.pl");
 			
+			/* executes the query file on the program file with Prolog interpreter */
 			ItfPrologInterpret pexec = new PrologExecutor();
 			List res = pexec.executePrologQuery(queryFile.getAbsolutePath(), programFile.getAbsolutePath());
 			
-			Map resultMap=new HashMap();
+			Set<Map> resultSet=new HashSet<Map>();
+			List<Map> tmpLst = new ArrayList<Map>();
+			int numMap;
+			boolean firstQuery = true;
+			
+			/*go through the List of results in order to generate
+			 * a List that will contain one map for each possibilities
+			 * of matching between the proxy names and the corresponding EObjects
+			 */
+			Iterator it1 = res.iterator();
+			while(it1.hasNext()){
+				//There is one List per request that are analyzed
+				List query = (List)it1.next();
+				
+				Iterator it2 = query.iterator();
+				numMap = 0;
+				while(it2.hasNext()){
+					//One list per predicates that returns some results
+					Map map;
+					List predicate = (List)it2.next();
+					
+					Iterator it3 = predicate.iterator();
+					String[] matchVars = (String[])it3.next();
+					
+					//creation of one map per predicates
+					if(firstQuery){
+						map = new HashMap();
+					}else{
+						map = tmpLst.get(numMap);
+					}
+					map.put(matchVars[0], id2obj.get(matchVars[1]));
+					tmpLst.add(numMap, map);
+					
+					numMap++;
+				}
+				firstQuery = false;
+			}
+			
+			/* the set of maps is created from the temporary list of maps */
+			Iterator lstIt = tmpLst.iterator();
+			while(lstIt.hasNext()){
+				resultSet.add((Map)lstIt.next());
+			}
 			
 			/* go through result List of the prolog interpretation
 			 * check if each pattern has a name and match in a new map
