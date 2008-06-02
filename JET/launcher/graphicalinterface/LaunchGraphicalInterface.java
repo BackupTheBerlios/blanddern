48aba63a5ce9aa32a10cb58f85fd8766672b1d8a
sutterl <sutterl>
2008-06-02 11:51:18 +0000

    New version of how you can launch the graphical interface

--- JET/launcher/graphicalinterface/LaunchGraphicalInterface.java
+++ JET/launcher/graphicalinterface/LaunchGraphicalInterface.java
@@ -1,5 +1,8 @@
 package graphicalinterface;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LaunchGraphicalInterface {
 
 	/**
@@ -8,10 +11,13 @@ public class LaunchGraphicalInterface {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		GraphicalInterface a = new GraphicalInterface("D:\\ENSISA\\PROJET2A\\workspace");
-        a.show();
+        a.setVisible(true);
         //We test if the paths exist
-        if(a.getPaths().size()!=0){
-        	System.out.println("paths");
+        if(a.getPaths().size()==5){
+        	ArrayList lst = (ArrayList) a.getPaths();
+        	for(int i=0;i<5;i++){
+        		System.out.println("lst["+i+"] = "+lst.get(i).toString());
+        	}
         }else{
         	System.out.println("paths does not exist");
         }
