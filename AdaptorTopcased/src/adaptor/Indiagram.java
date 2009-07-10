eb148bbd96580f5ab1d267b4b5f6fabad2c01d73
bcoppe <bcoppe>
2009-07-10 17:34:59 +0000

    *** empty log message ***

---
+++ AdaptorTopcased/src/adaptor/Indiagram.java
@@ -0,0 +1,22 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id: Indiagram.java,v 1.1 2009/07/10 17:34:59 bcoppe Exp $
+ */
+package adaptor;
+
+import org.eclipse.emf.ecore.EObject;
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Indiagram</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see adaptor.AdaptorPackage#getIndiagram()
+ * @model abstract="true"
+ * @generated
+ */
+public interface Indiagram extends EObject {
+} // Indiagram
---
+++ AdaptorTopcased/src/adaptor/impl/IndiagramImpl.java
@@ -0,0 +1,45 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id: IndiagramImpl.java,v 1.1 2009/07/10 17:36:10 bcoppe Exp $
+ */
+package adaptor.impl;
+
+import adaptor.AdaptorPackage;
+import adaptor.Indiagram;
+
+import org.eclipse.emf.ecore.EClass;
+
+import org.eclipse.emf.ecore.impl.EObjectImpl;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Indiagram</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * </p>
+ *
+ * @generated
+ */
+public abstract class IndiagramImpl extends EObjectImpl implements Indiagram {
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected IndiagramImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return AdaptorPackage.Literals.INDIAGRAM;
+	}
+
+} //IndiagramImpl
